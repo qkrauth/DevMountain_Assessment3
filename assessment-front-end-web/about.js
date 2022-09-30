6b6c584686dde576bab8990560b4390fbf9cff3b
Quinten Krauth <quintenkrauth19@gmail.com>
2022-09-30 11:29:01 -0400

    exam

--- assessment-front-end-web/about.js
+++ assessment-front-end-web/about.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("Form has been submitted just now! CONGRATS! HOW DID U SUBMIT A FORM?")
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+function hover(event) {
+	alert("Nice work, you hovered over the rubber duck!");
+}
+
+let form2 = document.querySelector("img");
+form2.addEventListener("mouseover", hover);
---
+++ assessment-front-end-web/profile.js
@@ -0,0 +1,21 @@
+function favColor(event) {
+    alert("Green!")
+}
+let color = document.querySelector("#color");
+color.addEventListener("click", favColor);
+
+
+
+function favPlace(event) {
+    alert("Home!")
+}
+let place = document.querySelector("#place");
+place.addEventListener("click", favPlace);
+
+
+
+function favRitual(event) {
+    alert("Gaming!")
+}
+let ritual = document.querySelector("#ritual");
+ritual.addEventListener("click", favRitual);
